a152988e6e16f0d097559c6ea013aa3ef676e506
Alberto Quiñonero Ayllon <albertoquinoneroayllon@MBPdeAlberto2.home>
2020-03-01 10:58:15 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,2 @@
-
-fetch("https://swapi.co/api/people/")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    let starWarsChar = myJson.results
-    console.log(starWarsChar)
-  });
-  
+let buttonLink
 
